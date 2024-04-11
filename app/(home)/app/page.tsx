ac1ac6a5cc90941f0c5312270227bba34bc70ed7
ayadag <109915071+ayadag@users.noreply.github.com>
2024-04-11 20:15:20 +0300

    Todo

--- app/(home)/app/page.tsx
+++ app/(home)/app/page.tsx
@@ -1,50 +1,58 @@
-import Image from "next/image";
+// import Image from "next/image";
 import Modal from '../../../components/Modal';
 
-const Index: React.FC = async () => {
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import { useTodo } from '../../../hooks/todo'
+import Loading from '../../../components/Loading'
+import TodoSection from '../../../components/todo/TodoSection'
+import styles from '../../../styles/Home.module.css'
 
-  return (
-    <div className='w-full h-screen flex justify-center items-center'>
-      {/* Intro Content */}
-      <div className="max-w-md  text-black dark:text-white rounded-lg shadow-lg border border-0.5 border-gray-300 dark:border-gray-800 p-[1.25rem]">
-        <div className="flex flex-col justify-center p-4 bg-inherit gap-10">
-          <div className='flex flex-row items-center justify-center gap-8'>
-            <a
-              href="https://builderz.build"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {" "}
-              <Image
-                src="/images/builderz-symbol.svg"
-                height={60}
-                width={50}
-                style={{
-                  objectFit: "contain",
-                }}
-                alt="builderz"
-              />
-            </a>
-            <Image
-              width={75}
-              height={75}
-              src="/sol.png"
-              className=''
-              alt="sol"
-            />
-          </div>
-          <h1 className="text-black dark:text-white">Hello Solana, meet Builderz.dev 👋</h1>
-          <p className={` text-black dark:text-white`}>
-            Explore what you can do with Builderz&rsquo; brand new{" "}
-            <b>Builderz Solana dApp Scaffold</b>
-          </p>
-          <div className="flex flex-col md:flex-row justify-start  items-center py-4">
-            <Modal />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+const Index: React.FC = async () => {
+        const { initialized, initializeUser, loading, transactionPending, completedTodos, incompleteTodos, addTodo, markTodo, removeTodo, markStaticTodo,removeStaticTodo, addStaticTodo, input,  handleChange } = useTodo()
+    
+    
+        return (
+            <div className={styles.container}>
+                <div className={styles.actionsContainer}>
+                    {initialized ? (
+                        <div className={styles.todoInput}>
+                            <div className={`${styles.todoCheckbox} ${styles.checked}`} />
+                            <div className={styles.inputContainer}>
+    
+    
+                            {/*<input value = {input} onChange={handleChange} id={styles.inputField} type="text" placeholder='Create a new todo...' />*/}
+                            <button type="button" className={styles.button} onClick={()=> addTodo()}>
+                            Create
+                            </button>
+                                {/* 
+                                <form onSubmit={()=> addTodo()}>
+                                    <input value = {input} onChange={handleChange} id={styles.inputField} type="text" placeholder='Create a new todo...' />
+                                </form>
+                                */}
+    
+                            </div>
+                            <div className={styles.iconContainer}>
+           
+                            </div>
+                        </div>
+                    ) : (
+                        <button type="button" className={styles.button} onClick={() => initializeUser()} disabled={transactionPending}>
+                            Initialize
+                        </button>
+                    )}
+                    <WalletMultiButton />
+                </div>
+    
+                <div className={styles.mainContainer}>
+                    <Loading loading={loading}>
+                        <TodoSection title="Tasks" todos={incompleteTodos} action={markTodo} />
+    
+                        <TodoSection title="Completed" todos={completedTodos} action={removeTodo} />
+                    </Loading>
+                </div>
+            </div>
+        )
+    }
+    
 
 export default Index;
