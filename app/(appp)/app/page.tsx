1369d04408bf0f574e03bbf57c23e23bd13d835e
ayadag <109915071+ayadag@users.noreply.github.com>
2024-04-12 08:38:26 +0300

    HW

--- app/(appp)/app/page.tsx
+++ app/(appp)/app/page.tsx
@@ -1,22 +1,28 @@
-"use client";
+// "use client";
 // import Image from "next/image";
 // import Modal from '../../../components/Modal';
 
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
-import { useTodo } from '../../../hooks/todo'
-import Loading from '../../../components/Loading'
-import TodoSection from '../../../components/todo/TodoSection'
-import styles from '../../../styles/Home.module.css'
+// import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+// import { useTodo } from '../../../hooks/todo'
+// import Loading from '../../../components/Loading'
+// import TodoSection from '../../../components/todo/TodoSection'
+// import styles from '../../../styles/Home.module.css'
 
 const Index: React.FC = async () => {
-        const { loading, completedTodos, incompleteTodos, markTodo, removeTodo } = useTodo()
+        // const { loading, completedTodos, incompleteTodos, markTodo, removeTodo } = useTodo()
     
     
         return (
-            <div className={styles.container}>    
-                <div className={styles.mainContainer}>
-                    <h1>Hello World!</h1>
-                </div>
+            <div className='w-full h-screen flex justify-center items-center '>
+
+            {/* Intro Content */}
+            <div className="max-w-md bg-transparent  text-black dark:text-white rounded-lg shadow-lg border border-0.5 border-gray-300 dark:border-gray-800 p-[1.25rem]">
+            <div className="flex flex-col justify-center p-4 bg-inherit gap-10">
+                
+                <h2>Hello World!</h2>
+
+            </div>
+            </div>
             </div>
         )
     }
