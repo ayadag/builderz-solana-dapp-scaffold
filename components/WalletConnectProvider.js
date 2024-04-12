3746ab025e12584898b5f77239a8bf7484d3916a
ayadag <109915071+ayadag@users.noreply.github.com>
2024-04-12 06:51:15 +0300

    'use client';

--- components/WalletConnectProvider.js
+++ components/WalletConnectProvider.js
@@ -1,3 +1,4 @@
+'use client';
 import { useMemo } from 'react'
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
